6071366f1d9b25905432b3e16de2521ecfbb3e9e
suzutatsu <suzuta1221@gmail.com>
2020-01-28 07:06:37 +0000

    コメント投稿機能及びdeviseのログイン機能の一部実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #devise
+  devise_for :users
+  get '/users/sign_out' => 'devise/sessions#destroy'
+
+
+  #homes
+  get '/' => 'homes#top'
+
+
+
+  #blogs
+  get 'blogs' => 'blogs#new'
+  post 'blogs' => 'blogs#create'
+  get 'index' => 'blogs#index'
+  get 'blogs/:id' => 'blogs#show', as: 'blog'
+  get 'blogs/:id/edit' => 'blogs#edit', as: 'edit_blog'
+  patch 'blogs/:id' => 'blogs#update', as:'update_blog'
+  delete 'blogs/:id' => 'blogs#destroy', as:'destroy_blog'
+
+
+
+
+
+
+
 end
